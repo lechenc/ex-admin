84244bb1e68ba393b29665568b729ea1b047d16a
lingbiao <flbmp16675361674@163.com>
2021-06-03 19:15:10 +0800

    6.3test_1

--- src/views/user/userlistDetail.vue
+++ src/views/user/userlistDetail.vue
@@ -22,9 +22,9 @@
           <div>注册时间：{{ current_row.registerTime }}</div>
           <div>最后登录IP：{{ current_row.loginIp }}</div>
           <div>直推邀请下级人数：{{ current_row.sonCount }}</div>
-          <div>所有总人数：{{ current_row.allSonCount }}</div>
+          <!-- <div>所有总人数：{{ current_row.allSonCount }}</div>
           <div>国家区号：{{ current_row.phoneAreaCode }}</div>
-          <div>昵称：{{ current_row.nickName }}</div>
+          <div>昵称：{{ current_row.nickName }}</div> -->
         </div>
         <h3>用户状态</h3>
         <div v-if="!isModify">
