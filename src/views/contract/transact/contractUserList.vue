843bdfbd20d9800d1dfa70a71542925e1453e26f
lingbiao <flbmp16675361674@163.com>
2021-07-07 15:03:11 +0800

    dev ok

--- src/views/contract/transact/contractUserList.vue
+++ src/views/contract/transact/contractUserList.vue
@@ -96,8 +96,6 @@ export default {
         params.createStartTime = befV.replace(/\//gi, '-');
         // 组件时间初始必须format格式
         this.searchCofig[0].value = [befV, nowV];
-      }else{
-        
       }
       if (this.search_params_obj.startTime) {
         let befV = this.search_params_obj.startTime.replace(/-/gi, '/');
