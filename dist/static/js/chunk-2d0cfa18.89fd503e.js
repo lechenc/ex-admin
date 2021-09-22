edfe9a2b421c523d20c8e137a8c64fc8df410fc3
lechen <1424825501@qq.com>
2021-09-22 15:30:46 +0800

    chore: dist

--- dist/static/js/chunk-2d0cfa18.89fd503e.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0cfa18"],{6514:function(e,n,c){"use strict";c.r(n);var t=function(){var e=this,n=e.$createElement,c=e._self._c||n;return c("router-view")},u=[],r=c("0c7c"),l={},a=Object(r["a"])(l,t,u,!1,null,null,null);n["default"]=a.exports}}]);
