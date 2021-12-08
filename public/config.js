acc7eb9db27c91f02e3edac42c35abf462a0b1c8
lingbiao <flbmp16675361674@163.com>
2021-12-08 14:49:06 +0800

    12.8 dev

--- public/config.js
+++ public/config.js
@@ -17,6 +17,10 @@ if (/flyex/.test(window.location.href)) {
   window.SERVER_PATH = 'https://admin.flyex.vip/api' // 测试接口地址.
 }
 
+if (/241/.test(window.location.href)) {
+  window.SERVER_PATH = 'http://192.168.0.241/api' // 开发接口地址.
+}
+
 // window.SERVER_PATH = 'http://47.57.21.80/api'; // 通用接口地址/
 // window.SERVER_PATH = 'http://192.168.3.45:16425'; // 通用接口地址
 
@@ -46,7 +50,7 @@ if (/dappmi/.test(window.location.href)) {
 
 // console.error('强制测试服务器')
 // window.SERVER_PATH = 'http://47.57.21.80/api'; // 通用接口地址/
-var urlPicTmp = /alpex|linkkn|test/.test(window.location.href) ? '' : '/'
+var urlPicTmp = /241|alpex|linkkn|test|uat/.test(window.location.href) ? '' : '/'
 window.SERVER_PATH_PIC = window.SERVER_PATH + '/admin/user/file/upload-file' // 通用接口地址/
 window.SERVER_PATH_FILE = window.SERVER_PATH + '/admin/user/upload/package'
 window.SERVER_PATH_SPECIAL_FILE =
