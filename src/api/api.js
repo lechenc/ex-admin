1d0b56bda2e470a395ac7d8a71c65d55d9cf4f41
fang lingbiao <friexin@alphawizard.org>
2022-01-10 11:07:51 +0800

    1.10 热钱包api修改

--- src/api/api.js
+++ src/api/api.js
@@ -3246,22 +3246,22 @@ export default class api {
 
   // 财务管理/热钱包提币管理 删除
   static apiDelHotWalletExtract(data) {
-    return Fetch.post('/account/withdrawconfig/del', data)
+    return Fetch.post('/admin/account/withdrawconfig/del', data)
   }
 
   // 财务管理/热钱包提币管理 列表
   static apiGetHotWalletExtractList(data) {
-    return Fetch.post('/account/withdrawconfig/find', data)
+    return Fetch.post('/admin/account/withdrawconfig/find', data)
   }
 
   // 财务管理/热钱包提币管理 添加
   static apiAddHotWalletExtract(data) {
-    return Fetch.post('/account/withdrawconfig/add', data)
+    return Fetch.post('/admin/account/withdrawconfig/add', data)
   }
 
   // 财务管理/热钱包提币管理 编辑
   static apiEditHotWalletExtract(data) {
-    return Fetch.post('/account/withdrawconfig/edit', data)
+    return Fetch.post('/admin/account/withdrawconfig/edit', data)
   }
 
   // 财务管理/热钱包提币管理 查看余额
